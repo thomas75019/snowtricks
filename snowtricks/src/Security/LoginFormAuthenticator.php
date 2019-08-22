62f55af10d998c0f0c4451448c199b29d073b29c
Thomas75019 <tlarousse3@gmail.com>
2019-08-22 21:19:38 +0200

    Handel email and password  error

--- snowtricks/src/Security/LoginFormAuthenticator.php
+++ snowtricks/src/Security/LoginFormAuthenticator.php
@@ -68,7 +68,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 
         if (!$user) {
             // fail authentication with a custom error
-            throw new CustomUserMessageAuthenticationException('Email could not be found.');
+            throw new CustomUserMessageAuthenticationException('Mauvais email.');
         }
 
         return $user;
@@ -76,6 +76,10 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 
     public function checkCredentials($credentials, UserInterface $user)
     {
+        if (!$this->passwordEncoder->isPasswordValid($user, $credentials['password']))
+        {
+            throw new CustomUserMessageAuthenticationException('Mauvais mot de passe.');
+        }
         return $this->passwordEncoder->isPasswordValid($user, $credentials['password']);
     }
 
