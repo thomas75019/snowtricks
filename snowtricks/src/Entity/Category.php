9dd4a3dfac04012a262dd8fdd1a337c507fb4a6a
Thomas75019 <tlarousse3@gmail.com>
2020-03-27 17:09:35 +0100

    Make test on Category entity

--- snowtricks/src/Entity/Category.php
+++ snowtricks/src/Entity/Category.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
@@ -17,6 +18,8 @@ class Category
     private $id;
 
     /**
+     * @Assert\NotBlank()
+     *
      * @ORM\Column(type="string", length=255)
      */
     private $name;
