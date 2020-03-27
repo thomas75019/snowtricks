bb8ac92debee4407aec96ead8a241592b8c267ec
Thomas75019 <tlarousse3@gmail.com>
2020-03-27 15:53:25 +0100

    Remake Trick entity Test

---
+++ snowtricks/tests/Entity/TrickTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use App\Entity\User;
+use App\Entity\Trick;
+use App\Entity\Category;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class TrickTest extends KernelTestCase
+{
+    private $trick;
+    protected function setUp()
+    {
+        self::bootKernel();
+    }
+
+    public function getEntity()
+    {
+        return (new Trick())
+            ->setName('test')
+            ->setUser(new User())
+            ->setSlug('test')
+            ->setCreatedAt(new \DateTime())
+            ->setDescription('test')
+            ->setUpdatedAt(new \DateTime())
+            ->setCategory(new Category())
+            ;
+    }
+
+    public function hasErrors(Trick $trick, $count = 0)
+    {
+        $errors = self::$container->get('validator')->validate($trick);
+
+        $this->assertCount($count, $errors);
+    }
+
+    public function testEntityIsValid()
+    {
+        $this->hasErrors($this->getEntity(), 0);
+    }
+
+    public function testEntityIsNotValid()
+    {
+        $trickWithErrors = $this->getEntity()
+            ->setName('');
+
+        $this->hasErrors($trickWithErrors, 1);
+    }
+}
